05be1e0242c37b35727463d3e137f4ea26070b26
rimpa basak <rimpabasak05@gmail.com>
2024-12-08 13:27:38 +0530

    add caculator

---
+++ caculator.js
@@ -0,0 +1,15 @@
+let input1 = 10;
+let input2 = 20;
+
+let result = input1 + input2;
+
+console.log("Input1: "+ input1 );
+console.log("Input2: "+ input2);
+console.log(input1 + " + " + input2 + " = "+ result);
+
+let a= "hello";
+let b = "world";
+
+let c = a + " " + b;
+
+console.log(c);
